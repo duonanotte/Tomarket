3834926b03fe8c345f7d1f5c7f96ab4041a50f35
duonanotte <166713796+duonanotte@users.noreply.github.com>
2024-11-05 15:43:06 +0300

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from typing import Optional
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
@@ -10,19 +10,24 @@ class Settings(BaseSettings):
     REF_ID: str = '00001N6y'
 
     USE_RANDOM_DELAY_IN_RUN: bool = False
-    RANDOM_DELAY_IN_RUN: list[int] = [0, 41000]
+    RANDOM_DELAY_IN_RUN: list[int] = [0, 300]
+
+    EMOJI_TO_SET: Optional[str] = "🍅"
 
     POINTS_COUNT: list[int] = [450, 600]
     AUTO_PLAY_GAME: bool = True
+
     AUTO_TASK: bool = True
+    AUTO_TASK_3RD: bool = True
+
     AUTO_DAILY_REWARD: bool = True
     AUTO_CLAIM_STARS: bool = True
     AUTO_CLAIM_COMBO: bool = True
-    AUTO_RANK_UPGRADE: bool = True
+    AUTO_RANK_UPGRADE: bool = False
 
-    USE_PROXY: bool = False
+    USE_PROXY: bool = True
 
-    SLEEP_TIME: list[int] = [21000, 32000]
+    SLEEP_TIME: list[int] = [32000, 42000]
 
 settings = Settings()
 
